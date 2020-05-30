947a3df22252b1864fe6c01f2a4fe356975f4888
tanahara <s10.vermouth.4869@gmail.com>
2020-05-30 13:56:41 +0900

    font-awesomeの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("bootstrap/dist/js/bootstrap")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("bootstrap/dist/js/bootstrap");
 
+import "@fortawesome/fontawesome-free/js/all";
 
-
-require("trix")
-require("@rails/actiontext")
+require("trix");
+require("@rails/actiontext");
