8a5c5b5c2d2abe4ffd4fb8448db761665e8dabd8
tanahara <s10.vermouth.4869@gmail.com>
2020-05-30 15:47:26 +0900

    modelの関係とvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # :recoverable
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable, :trackable
-end
+enduser
