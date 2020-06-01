a0af845e779d6b7c5e1190b63b35c4a596795073
tanahara <s10.vermouth.4869@gmail.com>
2020-06-01 18:47:29 +0900

    devise新規登録機能削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  as :user do
+    get "users/edit" => "devise/registrations#edit", as: "edit_user_registration"
+    put "users" => "devise/registrations#update", as: "user_registration"
+  end
 end
